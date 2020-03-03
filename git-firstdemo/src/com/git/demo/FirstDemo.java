2a869ec60b5acf1094911183d73226ecba8cb12f
liaofangkun <18654139732@163.com>
2020-03-03 20:55:55 +0800

    第二次提交

--- git-firstdemo/src/com/git/demo/FirstDemo.java
+++ git-firstdemo/src/com/git/demo/FirstDemo.java
@@ -4,4 +4,8 @@ public class FirstDemo {
     public static void main(String[] args) {
         System.out.println("这是我的第一个git项目!!!");
     }
+
+    public void speak() {
+        System.out.println("test...");
+    }
 }
